cdc389d647bee960aa8c49695d17f60e1fb5512a
Derrick Stolee <dstolee@microsoft.com>
2020-08-26 09:17:30 -0400

    scalar: remove Watchman dependency
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Casks/scalar.rb
+++ Casks/scalar.rb
@@ -12,7 +12,6 @@ cask 'scalar' do
   conflicts_with cask: 'scalar-azrepos'
 
   depends_on formula: 'git'
-  depends_on formula: 'watchman'
   depends_on cask: 'git-credential-manager-core'
 
   uninstall script: {
