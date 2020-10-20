dd22283e584863f5ade1ceb6f41a68cf93a1e6c0
Matthew John Cheetham <mjcheetham@github.com>
2020-10-20 19:36:06 +0100

    microsoft-git: update to 2.29.0.vfs.0.0

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.28.0.vfs.0.0'
-  sha256 'c042d2abfbc4ec20231be7cf95e259aa84c97f97874413bfccfa8fa78bc56f5a'
+  version '2.29.0.vfs.0.0'
+  sha256 'b829b49daca5dcf7ad73028b332f68f5e5f9474282f72164ddaf8235e978a02e'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-intel-universal-snow-leopard.pkg"
 
