6e0a2c6ac204357d77787e2b247aa7404fd62d89
Matthew John Cheetham <mjcheetham@github.com>
2020-11-03 10:46:02 +0000

    git-credential-manager-core: update to 2.0.280.19487

--- Casks/git-credential-manager-core.rb
+++ Casks/git-credential-manager-core.rb
@@ -2,8 +2,8 @@ cask 'git-credential-manager-core' do
   name 'Git Credential Manager Core'
   homepage 'https://aka.ms/gcmcore'
 
-  version "2.0.252.766"
-  sha256 '3d7215127fa546b6b5b21fd5a49cde088d9e0fe9a5112a72a708e7649a7ac170'
+  version "2.0.280.19487"
+  sha256 '1cdbb55e60df6526a0581b1d46f6fbe94e028d0f4a36b73fface16248a66499c'
 
   url "https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v#{version.major_minor_patch}-beta/gcmcore-osx-#{version}.pkg"
 
