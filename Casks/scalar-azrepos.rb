01fde7aa906a227137a0a58fca1d5cb3933a06e6
Matthew John Cheetham <mjcheetham@github.com>
2021-01-14 22:32:42 +0000

    scalar-azrepos: update to 21.01.182.4

--- Casks/scalar-azrepos.rb
+++ Casks/scalar-azrepos.rb
@@ -1,6 +1,6 @@
 cask 'scalar-azrepos' do
-  version '20.10.178.7'
-  sha256 'bb3e53d66c7db479f320c4169898dc9dd0fb6b0c3f37bf44cddf5b4345b9566e'
+  version '21.01.182.4'
+  sha256 'f8f8db7856646c5b23b91496fdb82565075fd971d2d176145bbcf36c952c634c'
 
   url "https://github.com/microsoft/scalar/releases/download/v#{version}/Installers_macOS_Release.zip"
 
