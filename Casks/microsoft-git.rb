61cf631677572a72bdb5991110bc7db2135175f7
Matthew John Cheetham <mjcheetham@github.com>
2022-07-26 17:54:51 +0100

    microsoft-git: update to 2.37.1.vfs.0.1

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.37.1.vfs.0.0'
-  sha256 'f8c504188a0eeb16abb5429aa5b26abed40fe1d3f2e8923384f998b00c22a656'
+  version '2.37.1.vfs.0.1'
+  sha256 '9fca489a5342fce19e8bb83da9cf9073d449666517008ad2dc2ecc5678d4c004'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-intel-x86_64-catalina.pkg"
 
