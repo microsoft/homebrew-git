695e1dfaea897fb1a0b41bf50210529e823ab81d
Lessley Dennington <ldennington@github.com>
2023-07-12 16:50:43 -0600

    git-credential-manager-core: update to 2.2.2

--- Casks/git-credential-manager-core.rb
+++ Casks/git-credential-manager-core.rb
@@ -3,11 +3,11 @@ cask 'git-credential-manager-core' do
   name 'Git Credential Manager'
   homepage 'https://aka.ms/gcm'
 
-  version "2.2.1"
+  version "2.2.2"
   if Hardware::CPU.intel?
-    sha256 '1aeb502844ec58028e9c6b35cd72a04ce81663a7546dbf3e3883f1e260e9b558'
+    sha256 '92f0d53a1042d3f827f199eb05b3e0041e9af6983f2fe5929d6d44f11eecde54'
   else
-    sha256 '8fd80beee204e1e028924308944dc21e58d7cafb3f3a83ca071a5544eefae22a'
+    sha256 '2a51a513ae4dc7f5221b4708bce9c7dd6444aa6ba133def5354ace7382a55c15'
   end
 
   url "https://github.com/GitCredentialManager/git-credential-manager/releases/download/v#{version.major_minor_patch}/gcm-osx-#{arch}-#{version.major_minor_patch}.pkg"
