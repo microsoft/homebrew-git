3dbe959af144d85e94bb14f10fb9f908e0aa38ee
Kyle Rader <126627085+kyle-rader-msft@users.noreply.github.com>
2023-08-17 09:01:58 -0700

    Use full cask path to GCM

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -11,7 +11,7 @@ cask 'microsoft-git' do
 
   conflicts_with formula: 'git'
   
-  depends_on cask: 'git-credential-manager'
+  depends_on cask: 'homebrew/cask/git-credential-manager'
 
   uninstall script: {
             executable: '/usr/local/git/uninstall.sh',
