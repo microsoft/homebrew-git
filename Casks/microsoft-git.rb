b608d274f75c60605cbc921c34a0007fed86f2db
Lessley Dennington <ldennington@github.com>
2024-03-22 07:49:43 -0600

    microsoft-git: update to 2.44.0.vfs.0.1

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.43.0.vfs.0.0'
-  sha256 '79b910dec102bc9493c19e18a1b6e620b00d6a36f134d3795b96c4a528cac49b'
+  version '2.44.0.vfs.0.1'
+  sha256 'bc537a6061c4a160d62c5b258e95c1cd6db469fae9cbd531be46340073d8fe5b'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-universal.pkg"
 
