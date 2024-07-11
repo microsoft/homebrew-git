b414a12e65510f994a91aa3e56a8cef23c1c849a
Johannes Schindelin <dscho@github.com>
2024-07-11 09:39:44 +0200

    microsoft-git: update to 2.45.2.vfs.0.2

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.45.2.vfs.0.1'
-  sha256 'cd345364ea8502e50f144c89ab470e7355d422282f9218d7e1bd8a57b30faf45'
+  version '2.45.2.vfs.0.2'
+  sha256 'afc0e98634b47ab96da3a3a121b0b18039a0c5b01fd83b87f6d43c945e093549'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-universal.pkg"
 
