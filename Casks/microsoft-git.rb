70f3e7d68daf34ac3204ad2d2b2cff307e5db7f4
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-12-09 17:28:48 +0000

    microsoft-git: update to 2.51.2.vfs.0.1

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.51.2.vfs.0.0'
-  sha256 '0a8787a31d1b3de78149efdecd126898c67b0905d09339652eea95212dad19a5'
+  version '2.51.2.vfs.0.1'
+  sha256 '421599141738ae20b8cd16e0ef2cfb467439183b9fb16a0e206e142f25f839b5'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-universal.pkg"
 
