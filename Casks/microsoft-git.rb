fa40231a2aaa88bab1c13b14e79cce433a347998
Johannes Schindelin <johannes.schindelin@gmx.de>
2026-08-06 15:00:07 +0200

    microsoft-git: update to 2.55.0.vfs.0.6

--- Casks/microsoft-git.rb
+++ Casks/microsoft-git.rb
@@ -2,8 +2,8 @@ cask 'microsoft-git' do
   name 'Microsoft-fork of Git'
   homepage 'https://github.com/microsoft/git'
 
-  version '2.55.0.vfs.0.3'
-  sha256 'c6ed4a1f157d96aa8a1b0afcd61b70f3de23f6ff739360c933a9955487095fa1'
+  version '2.55.0.vfs.0.6'
+  sha256 '38829bd9adbbb98f334766bc5dbe6c996fb457941dcffb5908bf34a48f3d9927'
 
   url "https://github.com/microsoft/git/releases/download/v#{version}/git-#{version}-universal.pkg"
 
